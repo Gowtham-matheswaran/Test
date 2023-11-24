352a60df7e38eb39250daae99b68a1bf0cf9eb54
Gowtham-matheswaran <139969985+Gowtham-matheswaran@users.noreply.github.com>
2023-11-24 13:07:42 +0530

    Added window handling code

--- src/com/org/reuse/NewClass.java
+++ src/com/org/reuse/NewClass.java
@@ -1,5 +1,20 @@
 package com.org.reuse;
 
-public class NewClass {
+import java.util.Set;
 
+import org.openqa.selenium.WebDriver;
+
+public class NewClass {
+	
+	public static WebDriver driver;
+	
+	public static void windowHandle() {
+		String parent = driver.getWindowHandle();
+		Set<String> child = driver.getWindowHandles();	
+		for(String base:child) {
+			if(!base.equals(parent)) {
+				driver.switchTo().window(base);
+			}
+		}
+}
 }
